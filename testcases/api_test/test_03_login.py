45fc5e18268301ea3d575ea789eaaacc78f881c1
cw <78747392@qq.com>
2022-09-09 19:44:08 +0800

    11d1

--- testcases/api_test/test_03_login.py
+++ testcases/api_test/test_03_login.py
@@ -34,7 +34,7 @@ class TestUserLogin():
         # logger.info("code ==>> 期望结果：{}， 实际结果：【 {} 】".format(except_code, result.response.json().get("code")))
         # assert result.response.json().get("code") == except_code
         # assert except_msg in result.msg
-        # logger.info("*************** 结束执行用例 ***********data={account:username,password:password}****")
+        logger.info("*************** 结束执行用例 ***********data={account:username,password:password}****")
 
 
 if __name__ == '__main__':
